045da75b66cf4f166fda6289832d1f16f65cb04e
antdkhtr <antd3149@gmail.com>
2021-06-17 11:28:08 +0300

    add Sort Popup

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const Categories = (props) => {
 
-    const {items, onClickItem, activeItem} = props
+    const {items} = props
+
+    const [activeItem, setActiveItem] = useState(null)
+    const onClickItem = (index) => {
+        setActiveItem(index)
+    }
     
     return (
         <div className="categories">
         <ul>
         <li className={activeItem === null ? "active" : "" } onClick={()=> onClickItem(null)}>Все</li>
-        {items && items.map((it,indx) => (
-        <li className={activeItem === indx ? "active" : "" } onClick={()=> onClickItem(indx)} 
-        key={`${indx}_${it}`}>{it}</li>))}
+        {items && items.map((it,index) => (
+        <li className={activeItem === index ? "active" : "" } onClick={()=> onClickItem(index)}
+        key={`${index}_${it}`}>{it}</li>))}
         </ul>
       </div>
     )
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {Button} from './Button'
 export {Categories} from './Categories'
 export {Header} from './Header'
+export {SortPopup} from './SortPopup'
