be1e460c95698e8aba5dab9b8017eeb638a72366
antdkhtr <antd3149@gmail.com>
2021-06-24 22:20:21 +0300

    add react-router-dom 4

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ import classNames from 'classnames'
 export const Button = (props) => {
 
     const {className, outline, children, onClick} = props
-  
+
     return (
         <button onClick={onClick} className={classNames('button', className, {
             'button--outline': outline,
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -8,16 +8,16 @@ export const Categories = (props) => {
     const onClickItem = (index) => {
         setActiveItem(index)
     }
-    
+
     return (
         <div className="categories">
-        <ul>
-        <li className={activeItem === null ? "active" : "" } onClick={()=> onClickItem(null)}>Все</li>
-        {items && items.map((it,index) => (
-        <li className={activeItem === index ? "active" : "" } onClick={()=> onClickItem(index)}
-        key={`${index}_${it}`}>{it}</li>))}
-        </ul>
-      </div>
+            <ul>
+                <li className={activeItem === null ? "active" : "" } onClick={()=> onClickItem(null)}>Все</li>
+                {items && items.map((it,index) => (
+                    <li className={activeItem === index ? "active" : "" } onClick={()=> onClickItem(index)}
+                        key={`${index}_${it}`}>{it}</li>))}
+            </ul>
+        </div>
     )
 }
 
