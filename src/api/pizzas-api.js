d424bd3a6ff546bf70bcbb3008494e178454516e
antdkhtr <antd3149@gmail.com>
2021-07-19 00:07:31 +0300

    add cart reducer

--- src/api/pizzas-api.js
+++ src/api/pizzas-api.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 export const pizzasApi = {
     getPizzas(){
-        return axios.get('http://localhost:3001/pizzas')
+        return axios.get('/pizzas')
     },
     getSortPizza(sortType, category){
         console.log(sortType, category)
-        return axios.get(`http://localhost:3001/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortType.type}&_order=${sortType.order}`)
+        return axios.get(`/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortType.type}&_order=${sortType.order}`)
     },
     changeCategory(category){
-        return axios.get(`http://localhost:3001/pizzas?category=${category}`)
+        return axios.get(`/pizzas?category=${category}`)
     }
 }
